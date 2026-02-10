931a19021572f654b3ce002f7b9c247c3a7d0d73
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-02-10 19:45:12 +0545

    predict

--- first.py
+++ first.py
@@ -3,3 +3,26 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error ,r2_score
 
+
+data = {
+"Size_sqft": [1000,1200,1500,1800,2000],
+"Price_k": [200,220,270,310,340]
+}
+df = pd.DataFrame(data)
+# print(df)
+
+#features and target
+X = df[["Size_sqft"]]#features
+y = df["Price_k"] #target
+
+#split Data :
+X_train, X_test ,y_train,y_test =train_test_split(X,y , test_size=0.2 ,random_state=42)
+
+#train Model
+model  = LinearRegression()
+model.fit(X_train,y_train)
+
+#preditct 
+y_pred = model.predict(X_test)
+print(y_pred)
+print(y_test)
