d8308de4a1afeb202b23041a988ffcca38fb2a19
Suresh Pun Magar <sureshmagar6256407@gmail.com>
2026-02-12 19:59:21 +0545

    u

--- prctice.py
+++ prctice.py
@@ -257,3 +257,9 @@ print(f"r2 score is {r2_score(y_test , y1)}")
 # loaded_file = jlb.load("Firstml.pkl")
 # y1 = loaded_file.predict([[6000,4]])
 # print(y1)
+
+import pandas as pd 
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression 
+from sklearn.metrics import mean_squared_error , r2_score  
+
